6e5ad75c6e40dfa2899056aaccca401f7cc1b8c1
fernandocgomez <fernandocgomez@live.com>
2025-04-27 22:34:38 -0500

    done with the generate component function

--- src/common-capabilities/generate-component/generate-component.ts
+++ src/common-capabilities/generate-component/generate-component.ts
@@ -1,19 +1,105 @@
-import { promptInput } from '@extensionFramework';
+import {
+	openTextFile,
+	promptBoolean,
+	promptInput,
+	showErrorMessage,
+	showInformationMessage,
+} from '@extensionFramework';
+import { isKebabCase, kebabCaseToPascal } from '@utils';
+import * as path from 'path';
+import * as fs from 'fs';
+import {
+	getCustomTemplatePath,
+	getDefaultTemplatePath,
+	renderTemplate,
+} from '@templates';
+import { generateComponentSpec } from '../generate-component-spec/generate-component-spec';
 
-// fsPath === '/home/fernando/test/src/app'
-export const generateComponent = async (fsPath: string) => {
-	// Ask for prefix
-	const componentSelectorPrefix = await promptInput({
+interface ComponentTemplateData {
+	className: string;
+	selector: string;
+	componentNameAsKebabCase: string;
+}
+
+/**
+ *
+ * @param fsPath /home/fernando/test/src/app
+ */
+export const generateComponent = async (fsPath: string): Promise<void> => {
+	const needSelectorPrefix = await promptBoolean({
+		prompt: 'Do you want to prefix your component selector?',
+		options: ['Yes', 'No'],
+	});
+
+	const componentSelectorPrefix = needSelectorPrefix
+		? await promptInput({
+				prompt: 'Enter component prefix (kebab-case)',
+				placeHolder: 'e.g. dashboard',
+				validationFn: value =>
+					isKebabCase(value)
+						? null
+						: 'Component prefix must be in kebab-case format',
+				errorMessage: 'Error collecting component prefix',
+			})
+		: null;
+
+	const nameInKebabCase = await promptInput({
 		prompt: 'Enter component name (kebab-case)',
 		placeHolder: 'e.g. user-profile',
-		validationFn: value => {
-			// Validate kebab-case format
-			return /^[a-z][a-z0-9]*(-[a-z0-9]+)*$/.test(value)
-				? null
-				: 'Component name must be in kebab-case format';
-		},
+		validationFn: value =>
+			isKebabCase(value) ? null : 'Component name must be in kebab-case format',
 		errorMessage: 'Error collecting component name',
 	});
 
-	console.log(componentSelectorPrefix);
+	if (!nameInKebabCase) {
+		showErrorMessage('Please provide a component name');
+
+		return;
+	}
+
+	const componentTemplateData: ComponentTemplateData = {
+		className: `${kebabCaseToPascal(nameInKebabCase)}`,
+		selector: needSelectorPrefix
+			? `${componentSelectorPrefix}-${nameInKebabCase}`
+			: `${nameInKebabCase}`,
+		componentNameAsKebabCase: nameInKebabCase,
+	};
+
+	try {
+		const componentFilePath = path.join(
+			fsPath,
+			`${nameInKebabCase}.component.ts`,
+		);
+		const customComponentTemplatePath = getCustomTemplatePath('component');
+
+		if (fs.existsSync(customComponentTemplatePath)) {
+			fs.writeFileSync(
+				componentFilePath,
+				renderTemplate(customComponentTemplatePath, componentTemplateData),
+			);
+		} else {
+			fs.writeFileSync(
+				componentFilePath,
+				renderTemplate(
+					getDefaultTemplatePath('component'),
+					componentTemplateData,
+				),
+			);
+		}
+
+		showInformationMessage('Component was generated successfully.');
+
+		if (
+			await promptBoolean({
+				prompt: 'Do you want to generate spec file?',
+				options: ['Yes', 'No'],
+			})
+		) {
+			await generateComponentSpec(componentFilePath);
+		}
+
+		await openTextFile(componentFilePath);
+	} catch (error: any) {
+		showErrorMessage(error.message);
+	}
 };
