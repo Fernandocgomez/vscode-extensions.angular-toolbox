cf37e24990cc30f6a58fbd0dd908467ae26cac9f
fernandocgomez <fernandocgomez@live.com>
2025-04-29 23:10:45 -0500

    add template file names to enum

--- src/models/enums/template-file-names.ts
+++ src/models/enums/template-file-names.ts
@@ -1,4 +1,8 @@
 export enum TemplateFileNames {
 	COMPONENT = 'component',
 	COMPONENT_SPEC = 'component.spec',
+	SERVICE = 'service',
+	SERVICE_SPEC = 'service.spec',
+	HTTP_SERVICE = 'http.service',
+	HTTP_SERVICE_SPEC = 'http.service.spec',
 }
