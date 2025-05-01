3dd6a74290ac7a74cf83bcd9517d5e2c1442d6c5
fernandocgomez <fernandocgomez@live.com>
2025-04-30 20:50:35 -0500

    define service spec template interface

--- src/models/interfaces/template-data.ts
+++ src/models/interfaces/template-data.ts
@@ -23,3 +23,9 @@ export interface ServiceTemplateData {
 	isGlobal: boolean;
 	className: string; // UserAuthService(HTTP) or Logger(None Http)
 }
+
+export interface ServiceSpecTemplateData {
+	className: string; // UserAuthService(HTTP) or Logger(None Http)
+	serviceFileName: string; // user-auth.service.ts or logger.service.ts
+	providers: Dependency[];
+}
