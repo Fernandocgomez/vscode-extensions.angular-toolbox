f718c974e103d2ecbfb22662ead9f9775b84798d
fernandocgomez <fernandocgomez@live.com>
2025-07-12 18:32:00 -0500

    chore (scripts): add release scripts for patch, minor, and major version updates

--- .scripts/release.js
+++ .scripts/release.js
@@ -4,6 +4,33 @@ const { execSync } = require('child_process');
 
 const main = () => {
 	try {
+		// --- Update package.json version ---
+		const packageJsonPath = path.join(__dirname, '..', 'package.json');
+		const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf8'));
+
+		const versionParts = packageJson.version.split('.').map(Number);
+
+		// Determine version bump type from command line argument: major, minor, patch (default: patch)
+		const bumpType = process.argv[2] || 'patch';
+		if (bumpType === 'major') {
+			versionParts[0]++;
+			versionParts[1] = 0;
+			versionParts[2] = 0;
+		} else if (bumpType === 'minor') {
+			versionParts[1]++;
+			versionParts[2] = 0;
+		} else {
+			versionParts[2]++;
+		}
+
+		const newVersion = versionParts.join('.');
+		packageJson.version = newVersion;
+		fs.writeFileSync(
+			packageJsonPath,
+			JSON.stringify(packageJson, null, 2) + '\n',
+		);
+		console.log(`Updated package.json version to ${newVersion}`);
+
 		// --- Install Node Dependencies ---
 		console.log('Installing dependencies...');
 		execSync('npm install', { stdio: 'inherit' });
